025a59ce32e51298999acb5ab29e27d950676479
name-mun <tains9153@gmail.com>
2024-10-31 20:00:14 +0900

    ✨ Calculator 기능 구현

---
+++ W2Project.playground/Contents.swift
@@ -0,0 +1,20 @@
+class Calculator {
+    func calculate(operatorText: String, firstNumber: Int, secondNumber: Int) -> Int {
+        switch operatorText {
+        case "+":
+            return firstNumber + secondNumber
+        case "-":
+            return firstNumber - secondNumber
+        case "*":
+            return firstNumber * secondNumber
+        case "/":
+            return firstNumber / secondNumber
+        default:
+            return 0
+        }
+
+    }
+}
+
+let calculator = Calculator()
+let addResult = calculator.calculate(operatorText: "+", firstNumber: 10, secondNumber: 20)

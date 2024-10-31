568c1037ddb5e3ae121b12c26756d68feda18526
name-mun <tains9153@gmail.com>
2024-10-31 22:19:52 +0900

    🚨 나머지 연산 예외처리 추가

--- W2Project.playground/Contents.swift
+++ W2Project.playground/Contents.swift
@@ -68,9 +68,16 @@ class DivideOperation: AbstractOperation {
 // 나머지 연산 기능을 제공하는 클래스
 class RemainderOperation: AbstractOperation {
     func operation(_ firstNumber: Int, _ secondNumber: Int) -> Int {
-        return firstNumber % secondNumber
+        
+        // 두번째 매개변수가 0일 경우 예외 처리
+        if secondNumber == 0 {
+            return -1
+        } else {
+            return firstNumber % secondNumber
+        }
     }
 }
 
 let calculator = Calculator()
 let addResult = calculator.calculate(operatorText: "+", firstNumber: 10, secondNumber: 20)
+

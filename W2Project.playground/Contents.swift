18071bfb6449c50ce3171c0f3f0a0a3f74d23b6b
name-mun <tains9153@gmail.com>
2024-10-31 21:41:01 +0900

    🎨 Calculator 메소드의 기능을 class로 구현

--- W2Project.playground/Contents.swift
+++ W2Project.playground/Contents.swift
@@ -1,20 +1,63 @@
 class Calculator {
+    
+    // 연산 인스턴스 생성
+    let addOperation = AddOperation()
+    let subtractOperation = SubtractOperation()
+    let multiplyOperation = MultiplyOperation()
+    let divideOperation = DivideOperation()
+    let remainderOperation = RemainderOperation()
+    
     func calculate(operatorText: String, firstNumber: Int, secondNumber: Int) -> Int {
         switch operatorText {
         case "+":
-            return firstNumber + secondNumber
+            return addOperation.add(firstNumber, secondNumber)
         case "-":
-            return firstNumber - secondNumber
+            return subtractOperation.subtract(firstNumber, secondNumber)
         case "*":
-            return firstNumber * secondNumber
+            return multiplyOperation.multiply(firstNumber, secondNumber)
         case "/":
-            return firstNumber / secondNumber
+            return divideOperation.divide(firstNumber, secondNumber)
         case "%":
-            return firstNumber % secondNumber
+            return remainderOperation.remainder(firstNumber, secondNumber)
         default:
             return 0
         }
 
+    }
+}
+
+// 더하기 연산 기능을 제공하는 클래스
+class AddOperation {
+    func add(_ firstNumber: Int, _ secondNumber: Int) -> Int {
+        return firstNumber + secondNumber
+    }
+}
+
+// 빼기 연산 기능을 제공하는 클래스
+class SubtractOperation {
+    func subtract(_ firstNumber: Int, _ secondNumber: Int) -> Int {
+        return firstNumber - secondNumber
+    }
+}
+
+// 곱하기 연산 기능을 제공하는 클래스
+class MultiplyOperation {
+    func multiply(_ firstNumber: Int, _ secondNumber: Int) -> Int {
+        return firstNumber * secondNumber
+    }
+}
+
+// 나누기 연산 기능을 제공하는 클래스
+class DivideOperation {
+    func divide(_ firstNumber: Int, _ secondNumber: Int) -> Int {
+        return firstNumber / secondNumber
+    }
+}
+
+// 나머지 연산 기능을 제공하는 클래스
+class RemainderOperation {
+    func remainder(_ firstNumber: Int, _ secondNumber: Int) -> Int {
+        return firstNumber % secondNumber
     }
 }
 

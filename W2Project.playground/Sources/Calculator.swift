84be9ae6afd491fee1e1305f1596100b9f6995d5
name-mun <tains9153@gmail.com>
2024-11-17 10:59:54 +0900

    🔧 파일을 모듈별로 분리

--- W2Project.playground/Sources/Calculator.swift
+++ W2Project.playground/Sources/Calculator.swift
@@ -5,3 +5,18 @@
 //  Created by mun on 11/17/24.
 //
 
+public class Calculator {
+    
+    // 연산 인스턴스 생성
+    public var operation: AbstractOperation?
+    
+    public init(operation: AbstractOperation? = nil) {
+        self.operation = operation
+    }
+    
+    public func calculate(firstNumber: Int, secondNumber: Int) -> Int {
+        (operation?.operation(firstNumber, secondNumber))!
+
+    }
+}
+

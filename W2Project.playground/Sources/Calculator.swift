2e7460d6411866887aa42dcdb64947b1d3b8b8d6
name-mun <tains9153@gmail.com>
2024-11-17 10:59:27 +0900

    🔧 파일을 모듈별로 분리

---
+++ W2Project.playground/Sources/Calculator.swift
@@ -0,0 +1,7 @@
+//
+//  asdf.swift
+//  
+//
+//  Created by mun on 11/17/24.
+//
+
---
+++ W2Project.playground/Sources/Operation.swift
@@ -0,0 +1,71 @@
+//
+//  AbstractOperation.swift
+//  
+//
+//  Created by mun on 11/17/24.
+//
+public protocol AbstractOperation {
+    func operation(_ firstNumber: Int, _ secondNumber: Int) -> Int
+}
+
+// 더하기 연산 기능을 제공하는 클래스
+public class AddOperation: AbstractOperation {
+    required public init() {}
+
+    public func operation(_ firstNumber: Int, _ secondNumber: Int) -> Int {
+        return firstNumber + secondNumber
+    }
+}
+
+// 빼기 연산 기능을 제공하는 클래스
+/**
+ - parameters:
+ - firstNumber:뺴기 연산자의 왼쪽에 오는 수
+ - secondNumber: 뺴기 연산자의 오른쪽에 오는 수
+ */
+public class SubtractOperation: AbstractOperation {
+    required public init() {}
+    
+    public func operation(_ firstNumber: Int, _ secondNumber: Int) -> Int {
+        return firstNumber - secondNumber
+    }
+}
+
+// 곱하기 연산 기능을 제공하는 클래스
+public class MultiplyOperation: AbstractOperation {
+    required public init() {}
+    
+    public func operation(_ firstNumber: Int, _ secondNumber: Int) -> Int {
+        return firstNumber * secondNumber
+    }
+}
+
+// 나누기 연산 기능을 제공하는 클래스
+public class DivideOperation: AbstractOperation {
+    required public init() {}
+    
+    public func operation(_ firstNumber: Int, _ secondNumber: Int) -> Int {
+        
+        // 매개변수가 0일 경우 예외 처리
+        if firstNumber == 0 || secondNumber == 0 {
+            return -1
+        } else {
+            return firstNumber / secondNumber
+        }
+    }
+}
+
+// 나머지 연산 기능을 제공하는 클래스
+public class RemainderOperation: AbstractOperation {
+    required public init() {}
+    
+    public func operation(_ firstNumber: Int, _ secondNumber: Int) -> Int {
+        
+        // 두번째 매개변수가 0일 경우 예외 처리 -> guard로 바꾸기
+        if secondNumber == 0 {
+            return -1
+        } else {
+            return firstNumber % secondNumber
+        }
+    }
+}
